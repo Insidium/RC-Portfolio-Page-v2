f1edffba763c58b1a6666cfe9172d10d3a0da7aa
Ryan Challen <ryanchallen@outlook.com>
2018-09-05 00:01:23 +1000

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuBranding = document.querySelector(".menu-branding");
 const menuNav = document.querySelector(".menu-nav");
-const navItem = document.querySelectorAll(".nav-item");
+const navItems = document.querySelectorAll(".nav-item");
 
 // Set Initial State Of menu
 let showMenu = false;
@@ -17,16 +17,16 @@ function toggleMenu() {
     menu.classList.add("show");
     menuBranding.classList.add("show");
     menuNav.classList.add("show");
-    navItem.forEach(item => item.classList.add("show"));
+    navItems.forEach(item => item.classList.add("show"));
 
     // Set Menu State
     showMenu = true;
   } else {
-    menuBtn.classList.add("close");
-    menu.classList.add("show");
-    menuBranding.classList.add("show");
-    menuNav.classList.add("show");
-    navItem.forEach(item => item.classList.remove("show"));
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuBranding.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
 
     // Set Menu State
     showMenu = false;
